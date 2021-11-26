82e4b94de2231bc0aad070d910d3dea26af3cad2
novakge <jsmeqd@gmail.com>
2021-11-26 10:26:44 +0100

    harmonize comments and naming

--- indicators_resource.m
+++ indicators_resource.m
@@ -1,9 +1,11 @@
 % RESOURCE INDICATORS without scheduling and for an earliest start schedule 
 
 % Prerequisites: (invoked in this function) 
-% tptfast.m     [TPT,EST,EFT,LST,LFT]=tptfast(DSM,T)
+% tptfast.m [TPT,EST,EFT,LST,LFT]=tptfast(DSM,T)
 % resfunc.m [BP,RESFUNC]=resfunc(DSM,SST,T,R)
-% ------------------------------------------------------------------------
+%
+% Example:
+% >> [RF,RU,PCTR,DMND,RS,RC,UTIL,TCON,OFACT,UFACT]=indicators_resource([1,1,24,2400,1,1;0,1,8,800,0,1],2,[-1,-1,1,1,3,1,1,9,1])
 
 %% RF: resource factor 
 % ~: the density of the resource matrix, that is a DMM
@@ -50,39 +52,39 @@
 
 function [RF,RU,PCTR,DMND,RS,RC,UTIL,TCON,OFACT,UFACT]=indicators_resource(PSM,num_r_resources,constr)
 n=size(PSM,1);
-%w=1;    % number of modes (w) is 1 (one) after the PDM has been decided, the PSM has been defined
-r=num_r_resources;  % number of renewable resources 
+%w=1; % number of modes (w) is 1 (one) after the PDM has been decided, the PSM has been defined
+r=num_r_resources; % number of renewable resources 
 LD=PSM(:,1:n);
-RD=PSM(:,n+2+1:n+2+r);    %resource domain, n-by-r matrix
-T=PSM(:,n+1);    %n-by-1 vector
-[TPT,SST]=tptfast(LD,T);    % total project time TPT (duration) - the length of the critical path - and scheduled sarting times SST using an earliest start schedule regardless the resource constraints
-[BP,RESFUNC]=resfunc(LD,SST,T,RD);   % resource loading function
+RD=PSM(:,n+2+1:n+2+r); %resource domain, n-by-r matrix
+TD=PSM(:,n+1); %n-by-1 vector
+[TPT,SST]=tptfast(LD,TD); % total project time TPT (duration) - the length of the critical path - and scheduled sarting times SST using an earliest start schedule regardless the resource constraints
+[BP,RESFUNC]=resfunc(LD,SST,TD,RD); % resource loading function
 
 
 %% INDICATORS without scheduling
 
-PCTR=sum(RD>0,1)/n; %percent of activities that require the given resuorce type
-RF=double(sum(PCTR,2)/r);   %resource factor
-RU=sum(RD>0,2); %resource use
-ak=constr(3:3+r-1); %1-by-r vector, the constraints are considered as constant during the project, the first 2 constraint are omitted: the first constraint belongs to time, the second belongs to cost
-DMND=sum(PSM(:,n+2+1:n+2+r),1)./sum(PSM(:,n+2+1:n+2+r)>0,1);    %average demand form each renewable resource
-RC=DMND./ak; %resource constrainedness for each resource type
-rkmin=max(PSM(:,n+2+1:n+2+r),[],1);   %1-by-r vector, maximum individual resource demand for each resource type
+PCTR=sum(RD>0,1)/n; % percent of activities that require the given resource type
+RF=double(sum(PCTR,2)/r); % resource factor
+RU=sum(RD>0,2); % resource use
+ak=constr(3:3+r-1); % 1-by-r vector, the constraints are considered as constant during the project, the first 2 constraint are omitted: the first constraint belongs to time, the second belongs to cost
+DMND=sum(PSM(:,n+2+1:n+2+r),1)./sum(PSM(:,n+2+1:n+2+r)>0,1); % average demand form each renewable resource
+RC=DMND./ak; % resource constrainedness for each resource type
+rkmin=max(PSM(:,n+2+1:n+2+r),[],1); % 1-by-r vector, maximum individual resource demand for each resource type
 
   
 %% INDICATORS for an earliest start SCHEDULE
-UTIL=(T'*RD)./(TPT*ak);
-rkmax=max(RESFUNC,[],1);    % maximum resource demand for each resource type 
+UTIL=(TD'*RD)./(TPT*ak);
+rkmax=max(RESFUNC,[],1); % maximum resource demand for each resource type 
 RS=(ak-rkmin)./(rkmax-rkmin);
 TCON=UTIL./sum(RD>0,1); % Resource Constrainedness Over Time
 ExResReq=0; % Excess Resource Requirement
-UU=0;   % Underutilization
+UU=0; % Underutilization
  for i=2:numel(BP)
      ExResReq=ExResReq+max(0,(RESFUNC(i)-ak))*(BP(i)-BP(i-1));
      UU=UU+max(0,-(RESFUNC(i)-ak))*(BP(i)-BP(i-1));
  end
-OFACT=ExResReq/(T'*RD);   % Obstruction factor of resources
+OFACT=ExResReq/(TD'*RD); % Obstruction factor of resources
 %TOTOFACT=sum(OFACT); % Total obstruction factor by Davis
 
-UFACT=UU/(T'*RD);    % Underutilization factor
+UFACT=UU/(TD'*RD); % Underutilization factor
 end
