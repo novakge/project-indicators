ea2266e0a4ee9aa1efda5d466f53a34e9b7ad1a5
novakge <jsmeqd@gmail.com>
2021-11-26 14:34:43 +0100

    add average indicators, fix O/UFACT calculation
    +add unit test
    +add test data

--- indicators_resource.m
+++ indicators_resource.m
@@ -5,7 +5,7 @@
 % resfunc.m [BP,RESFUNC]=resfunc(DSM,SST,T,R)
 %
 % Example:
-% >> [RF,RU,PCTR,DMND,RS,RC,UTIL,TCON,OFACT,UFACT]=indicators_resource([1,1,24,2400,1,1;0,1,8,800,0,1],2,[-1,-1,1,1,3,1,1,9,1])
+% >> [RF,RU,PCTR,DMND,XDMND,RS,RC,UTIL,XUTIL,TCON,XCON,OFACT,TOTOFACT,UFACT,TOTUFACT]=indicators_resource([1,1,24,2400,1,1;0,1,8,800,0,1],2,[-1,-1,1,1,3,1,1,9,1])
 
 %% RF: resource factor 
 % ~: the density of the resource matrix, that is a DMM
@@ -50,7 +50,7 @@
 
 
 
-function [RF,RU,PCTR,DMND,RS,RC,UTIL,TCON,OFACT,UFACT]=indicators_resource(PSM,num_r_resources,constr)
+function [RF,RU,PCTR,DMND,XDMND,RS,RC,UTIL,XUTIL,TCON,XCON,OFACT,TOTOFACT,UFACT,TOTUFACT]=indicators_resource(PSM,num_r_resources,constr)
 n=size(PSM,1);
 %w=1; % number of modes (w) is 1 (one) after the PDM has been decided, the PSM has been defined
 r=num_r_resources; % number of renewable resources 
@@ -65,26 +65,32 @@ TD=PSM(:,n+1); %n-by-1 vector
 
 PCTR=sum(RD>0,1)/n; % percent of activities that require the given resource type
 RF=double(sum(PCTR,2)/r); % resource factor
-RU=sum(RD>0,2); % resource use
+RU=sum(RD>0,2); % resource use 
+RU=mean(RU); % average version of Demeulemeester, 2003
 ak=constr(3:3+r-1); % 1-by-r vector, the constraints are considered as constant during the project, the first 2 constraint are omitted: the first constraint belongs to time, the second belongs to cost
-DMND=sum(PSM(:,n+2+1:n+2+r),1)./sum(PSM(:,n+2+1:n+2+r)>0,1); % average demand form each renewable resource
+DMND=sum(PSM(:,n+2+1:n+2+r),1)./sum(PSM(:,n+2+1:n+2+r)>0,1); % average demand from each renewable resource
+XDMND=mean(DMND); % average version of Patterson, 1976
 RC=DMND./ak; % resource constrainedness for each resource type
+RC=mean(RC); % average version of Patterson, 1976
 rkmin=max(PSM(:,n+2+1:n+2+r),[],1); % 1-by-r vector, maximum individual resource demand for each resource type
 
   
 %% INDICATORS for an earliest start SCHEDULE
 UTIL=(TD'*RD)./(TPT*ak);
+XUTIL=mean(UTIL); % average version of Patterson, 1976 (XUTIL)
 rkmax=max(RESFUNC,[],1); % maximum resource demand for each resource type 
-RS=(ak-rkmin)./(rkmax-rkmin);
+RS=(ak-rkmin)./(rkmax-rkmin); % resource strength
+RS=mean(RS); % average version of Kolisch, 1995
 TCON=UTIL./sum(RD>0,1); % Resource Constrainedness Over Time
+XCON=mean(TCON); % average version Patterson 1976 (XCON)
 ExResReq=0; % Excess Resource Requirement
 UU=0; % Underutilization
  for i=2:numel(BP)
      ExResReq=ExResReq+max(0,(RESFUNC(i)-ak))*(BP(i)-BP(i-1));
-     UU=UU+max(0,-(RESFUNC(i)-ak))*(BP(i)-BP(i-1));
+     UU=UU+max(0,(ak-RESFUNC(i))*(BP(i)-BP(i-1)));
  end
-OFACT=ExResReq/(TD'*RD); % Obstruction factor of resources
-%TOTOFACT=sum(OFACT); % Total obstruction factor by Davis
-
-UFACT=UU/(TD'*RD); % Underutilization factor
+OFACT=ExResReq./(TD'*RD); % Obstruction factor of resources
+TOTOFACT=sum(OFACT); % Total obstruction factor by Davis
+UFACT=UU./(TD'*RD); % Underutilization factor
+TOTUFACT=sum(UFACT); % Total Underutilization factor
 end
