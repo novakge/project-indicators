349ee1885a0731cd7da2d90fd8763fae97dbc6ca
novakge <jsmeqd@gmail.com>
2022-01-11 12:11:21 +0100

    add NARLF, NARLF', merge ARLF
    +unit tests

---
+++ indicator_narlf.m
@@ -0,0 +1,164 @@
+% Resource related indicator: (N)ARLF (Normalised) Average Resource Load Factor
+% input #1: PSM superset (Project Scheduling Matrix) for the renewable resources with (already decided), single mode (out of all available modes)
+% input #2: number of renewable resources
+% input #3: number of activities (gives structure of PDM superset and number of projects)
+% input #4: release dates, the time before the projects cannot start
+% example #1: >> [arlf,narlf,narlf_] = indicator_narlf(PSM, num_activities, num_r_resources, {release_dates})
+% example #2: >> [arlf,narlf,narlf_] = indicator_narlf(PSM, 4, [3,5,4], [0,0,0])
+% example #3: >> [arlf,narlf,narlf_] = indicator_narlf(PSM, 4, [3,5,4])
+
+% ARLF
+% description: value < 0 means resources are distributed front loaded, if > 0 resources are back loaded
+% r_ilk = demand of resource type k for task_i in project_l
+% K_il = number of resource types for task_i in project_l
+% N_l = number of tasks in project_l
+% original equation: 1 / CP_l * sum(t=1...CP_l) sum(k=1...K_il) sum(i=1...N_l) [ Z_ilt * X_ilt * (r_ilk / K_il) ]
+% Z_ilt =  { -1 if t <= CP_l/2
+%          { +1 if t >  CP_l/2
+% X_ilt =  { 1 if task_i of project_l is active at time t
+%          { 0 if task_i of project_l is inactive at time t
+% the ARLF of the multiproject/portfolio is obtained by averaging over all projects, i.e. divide by number of projects
+
+% NARLF by Browning & Yassine (2010b).
+% Improved version of ARLF, where critical paths of the individual projects are considered (CP_max) to decide if the resources are distributed in the first or in the second half of the projects.
+% Result is normalised over the CP of the multiproject/portfolio.
+
+% NARLF' by Van Eynde & Vanhoucke (2020) is an improved version
+% Similar to NARLF but considers the first or second half of the whole critical path duration instead of the individual projects critical path duration.
+
+% Remark: flexible activities must be handled before calling this indicator
+
+function [arlf,narlf,narlf_] = indicator_narlf(PSM, num_activities, num_r_resources, release_dates)
+
+% check (multi)project size based on num_activities
+num_projects = numel(num_activities);
+
+% get total number of activities
+n = size(PSM,1);
+
+% get number of renewable resources
+r = num_r_resources; % TODO: handle variable resource sizes
+
+% check if release_dates was provided as argument, if not, initialize with zeros for all projects
+if ~exist('release_dates', 'var')
+    release_dates = zeros(1,num_projects);
+end
+    
+% pre-calculate start-end offset of each project based on number of activities
+prj_starts = cumsum([1,num_activities(1:end-1)]); % starting with 1 for the first project, ignoring last entry
+prj_ends = cumsum(num_activities(1:end));
+
+% get single DSMs from the PSM superset
+DSM = {};
+for j = 1:num_projects
+    DSM{j} = PSM(prj_starts(j):prj_ends(j),prj_starts(j):prj_ends(j)); % get single DSMs for all project
+end
+
+% get single RDs and TDs from the PSM superset
+RD = {};
+TD = {};
+for j = 1:num_projects
+    RD{j} = PSM(prj_starts(j):prj_ends(j),n+2+1:n+2+r); % get single (n x r) resource domain matrices for all projects
+    TD{j} = PSM(prj_starts(j):prj_ends(j),n+1); % get single (n x 1) time domain matrices for all projects
+end
+
+% get DSM superset from PSM/PDM superset
+DSM_set = PSM(:,1:n); % get superset (n x n) design structure matrices
+TD_set = PSM(:,n+1); % get superset (n x 1) time domain matrices
+RD_set = PSM(:,n+2+1:n+2+r); % get superset (n x 1) resource domain matrices
+
+% calculate TPT (duration) as the length of the critical path (CP) and EST as earliest start schedule (time-only analysis)
+TPT = [];
+EST = [];
+[TPT,EST]=tptfast(DSM_set,TD_set); % for the whole (multi)project or portfolio
+
+% calculate TPT for each project for the old (N)ARLF version
+TPT_all = {};
+for j = 1:num_projects % for each project
+    [TPT_all{j},EST_all{j}] = tptfast(DSM{j},TD{j});
+end
+
+% TODO: take non-zero project release dates into account for the scheduling
+
+% ARLF original version
+arlf_all = {}; % pre-allocate storage for ARLF values
+for j=1:num_projects % for each project
+    
+    res_profile = zeros(1,TPT_all{j}); % initialize row vector for resource profile(s) for the actual project
+    
+    for k=1:num_r_resources % for all resources
+        
+        for i=1:num_activities(j) % for all tasks
+            
+            for t=EST_all{j}(i)+1:EST_all{j}(i)+TD{j}(i) % non-zero indexing e.g. if EST=0; TD=0 is skipped
+                
+                % ARLF original version
+                
+                if (t <= (release_dates(j) + TPT_all{j}/2)) % t is in first half, consider as negative
+                    % remark: +1 for non-zero release dates not used here
+                    % remark: rounding up TPT_j/2 is not used here as in Van Eynde (2020)
+                    res_profile(t) = res_profile(t) + RD{j}(i,k) * (-1); % subtract resource demand(s) of task_i;
+                else % t is in second half, consider as positive
+                    res_profile(t) = res_profile(t) + RD{j}(i,k); % add resource demand(s) of task_i;
+                end
+            end 
+        end
+    end
+
+    arlf_all{j} = sum(res_profile,'all') / double(r) / TPT_all{j}; % sum up resource profile for current project
+    
+end
+
+% NARLF original version
+resource_profile = zeros(1,TPT); % pre-allocate a row vector for resource profile(s), for all tasks
+for j=1:num_projects % for each project
+    for k=1:num_r_resources % for all resources
+        for i=1:num_activities(j) % for all tasks
+            for t=EST_all{j}(i)+1:EST_all{j}(i)+TD{j}(i) % non-zero indexing e.g. if EST=0; TD=0 is skipped
+                
+                % NARLF original version
+                
+                if (t <= (release_dates(j) + ceil(TPT_all{j}/2) + 1)) % t is in first half, consider as negative; +1 for non-zero release dates
+                    % remark: rounding up TPT/2 is not defined in the original equation (Browning et al., 2010), but used here to align with existing results in literature (Van Eynde, 2020). 
+                    resource_profile(t) = resource_profile(t) + RD{j}(i,k) * (-1); % subtract resource demand(s) of task_i;
+                else % t is in second half, consider as positive
+                    resource_profile(t) = resource_profile(t) + RD{j}(i,k); % add resource demand(s) of task_i;
+                end
+            end
+        end
+    end
+end
+
+
+% NARLF' improved version (here as NARLF_)
+% build complete resource profile as checking EST is not accurate enough
+resource_profile_ = zeros(1,TPT); % pre-allocate a row vector for resource profile(s), for all tasks
+for k=1:num_r_resources % for all resources
+    for i=1:n % for all tasks
+        for t=EST(i)+1:EST(i)+TD_set(i) % non-zero indexing e.g. if EST=0; TD=0 is skipped
+
+            if (t <= min(release_dates) + 1 + ceil(TPT/2)) % t is in first half, consider as negative; +1 for non-zero release dates
+                resource_profile_(t) = resource_profile_(t) + RD_set(i,k) * (-1); % subtract resource demand(s) of task_i;
+            else % t is in second half, consider as positive
+                resource_profile_(t) = resource_profile_(t) + RD_set(i,k); % add resource demand(s) of task_i;
+            end
+            
+        end
+    end
+end
+
+% hint: to debug the detailed resource function table, change "resource_profile = zeros(n,TPT)" and in the loop, use "resource_profile(i,t)" instead of the aggregated row vector of resource profiles
+
+% ARLF
+arlf = sum(cell2mat(arlf_all),'all') / (num_projects);
+
+% original NARLF
+narlf = sum(resource_profile,'all') / (num_projects * max(cell2mat(TPT_all)) * double(r));
+
+% improved NARLF'
+narlf_ = sum(resource_profile_,'all') / (num_projects * TPT * double(r));
+
+% check for NaN, Inf aftwerards and notify user
+if ( isinf(narlf) || isnan(narlf) )
+   warning('Warning: Inf or NaN value detected for indicator! (zero TPT?)\n');
+end
---
+++ indicator_narlf_test.m
@@ -0,0 +1,142 @@
+% https://www.mathworks.com/help/matlab/matlab-unit-test-framework.html
+% example#1: results = run(indicator_narlf_test)
+% example#2: runtests('indicator_narlf_test')
+
+function tests = indicator_narlf_test
+tests = functiontests(localfunctions);
+end
+
+function setupOnce(testCase)
+% setup global stuff
+end
+
+% setup test cases
+function setup(testCase)
+
+% load parsed data from *.mat file
+data = load('test_data/BY_10_1_14_NTP.mat', 'PDM', 'num_activities', 'num_r_resources');
+PSM = data.PDM;
+testCase.TestData.NARLF_001_1 = PSM;
+testCase.TestData.NARLF_001_2 = data.num_activities;
+testCase.TestData.NARLF_001_3 = data.num_r_resources;
+testCase.TestData.NARLF_002_1 = testCase.TestData.NARLF_001_1;
+testCase.TestData.NARLF_002_2 = testCase.TestData.NARLF_001_2;
+testCase.TestData.NARLF_002_3 = testCase.TestData.NARLF_001_3;
+
+% load parsed data from *.mat file
+data = load('test_data/BY_8_3_73_NTP.mat', 'PDM', 'num_activities', 'num_r_resources');
+PSM = data.PDM;
+testCase.TestData.NARLF_003_1 = PSM;
+testCase.TestData.NARLF_003_2 = data.num_activities;
+testCase.TestData.NARLF_003_3 = data.num_r_resources;
+
+testCase.TestData.NARLF_004_1 = testCase.TestData.NARLF_003_1;
+testCase.TestData.NARLF_004_2 = testCase.TestData.NARLF_003_2;
+testCase.TestData.NARLF_004_3 = testCase.TestData.NARLF_003_3;
+
+
+
+% load parsed data from *.mat file
+data = load('test_data/j301_10_NTP.mat', 'PDM', 'num_activities', 'num_r_resources');
+PSM = data.PDM;
+testCase.TestData.ARLF_001_1 = PSM;
+testCase.TestData.ARLF_001_2 = data.num_activities; % activities
+testCase.TestData.ARLF_001_3 = data.num_r_resources; % resources
+
+% project 1 example from literature: Kurtulus, I. (1985). Multiproject scheduling: Analysis of scheduling strategies under unequal delay penalties. Journal of Operations Management, 5(3), 291–307. doi:10.1016/0272-6963(85)90015-4 
+% CD is n/a (=NaN)
+testCase.TestData.ARLF_002_1 = [1,1,1,1,1,0,0,NaN,0;
+                                0,1,0,0,0,1,2,NaN,1;
+                                0,0,1,0,0,1,3,NaN,2;
+                                0,0,0,1,0,1,1,NaN,1;
+                                0,0,0,0,1,1,3,NaN,3;
+                                0,0,0,0,0,1,0,NaN,0];
+
+testCase.TestData.ARLF_002_2 = 6; % 6 activities
+testCase.TestData.ARLF_002_3 = 1; % 1 renewable resource
+
+
+% project 2 example from literature: Kurtulus, I. (1985). Multiproject scheduling: Analysis of scheduling strategies under unequal delay penalties. Journal of Operations Management, 5(3), 291–307. doi:10.1016/0272-6963(85)90015-4 
+% CD is n/a (=NaN)
+testCase.TestData.ARLF_003_1 = [1,1,0,0,0,0,0,NaN,0;
+                                0,1,1,1,1,0,3,NaN,1;
+                                0,0,1,0,0,1,1,NaN,2;
+                                0,0,0,1,0,1,3,NaN,3;
+                                0,0,0,0,1,1,2,NaN,1;
+                                0,0,0,0,0,1,0,NaN,0];
+testCase.TestData.ARLF_003_2 = 6; % 6 activities
+testCase.TestData.ARLF_003_3 = 1; % 1 renewable resource
+
+
+% own example #1 with a flexible structure
+%                              [ DSM         ][TD][CD ][RD ]
+% testCase.TestData.ARLF_004_1 = [1,1,0  ,0,0,0,1   ,NaN ,0,3;
+%                                0,1,0.8,0,0,0,4   ,NaN ,1,4;
+%                                0,0, 0 ,0,0,0,5   ,NaN ,2,1;
+%                                0,0,0  ,1,0,1,1   ,NaN ,2,0;
+%                                0,0,0  ,0,1,1,2   ,NaN ,2,1;
+%                                0,0,0  ,0,0,1,0   ,NaN ,1,1];
+% testCase.TestData.ARLF_004_2 = 2; % 2 renewable resources
+% testCase.TestData.ARLF_004_3 = 6; % 6 activities
+
+
+end
+
+% NARLF / NARLF_ tests
+function test_NARLF_001(testCase) % NTP
+[~,NARLF_act,~] = indicator_narlf(testCase.TestData.NARLF_001_1,testCase.TestData.NARLF_001_2,testCase.TestData.NARLF_001_3);
+NARLF_exp = -9.453;
+verifyEqual(testCase,NARLF_act,NARLF_exp,'AbsTol',0.001)
+end
+
+function test_NARLF_002(testCase) % NTP
+[~,~,NARLF_act] = indicator_narlf(testCase.TestData.NARLF_002_1,testCase.TestData.NARLF_002_2,testCase.TestData.NARLF_002_3);
+NARLF_exp = -12.223;
+verifyEqual(testCase,NARLF_act,NARLF_exp,'AbsTol',0.001)
+end
+
+function test_NARLF_003(testCase) % NTP
+[~,NARLF_act,~] = indicator_narlf(testCase.TestData.NARLF_003_1,testCase.TestData.NARLF_003_2,testCase.TestData.NARLF_003_3);
+NARLF_exp = -13;
+verifyEqual(testCase,NARLF_act,NARLF_exp,'AbsTol',0.001)
+end
+
+function test_NARLF_004(testCase) % NTP
+[~,~,NARLF_act] = indicator_narlf(testCase.TestData.NARLF_004_1,testCase.TestData.NARLF_004_2,testCase.TestData.NARLF_004_3);
+NARLF_exp = -14.342;
+verifyEqual(testCase,NARLF_act,NARLF_exp,'AbsTol',0.001)
+end
+
+% ARLF tests
+
+function test_ARLF_001(testCase) % NTP
+[ARLF_act,~,~] = indicator_narlf(testCase.TestData.ARLF_001_1,testCase.TestData.ARLF_001_2,testCase.TestData.ARLF_001_3);
+ARLF_exp = -3.262;
+verifyEqual(testCase,ARLF_act,ARLF_exp,'AbsTol',0.001)
+end
+
+function test_ARLF_002(testCase)
+[ARLF_act,~,~] = indicator_narlf(testCase.TestData.ARLF_002_1,testCase.TestData.ARLF_002_2,testCase.TestData.ARLF_002_3);
+ARLF_exp = 1.334;
+verifyEqual(testCase,ARLF_act,ARLF_exp,'AbsTol',0.001)
+end
+
+function test_ARLF_003(testCase)
+[ARLF_act,~,~] = indicator_narlf(testCase.TestData.ARLF_003_1,testCase.TestData.ARLF_003_2,testCase.TestData.ARLF_003_3);
+ARLF_exp = 1.667;
+verifyEqual(testCase,ARLF_act,ARLF_exp,'AbsTol',0.001)
+end
+
+%function test_ARLF_004(testCase)
+%[ARLF_act,~,~] = indicator_narlf(testCase.TestData.ARLF_004_1,testCase.TestData.ARLF_004_2,testCase.TestData.ARLF_004_3);
+%ARLF_exp = -0.1;
+%verifyEqual(testCase,ARLF_act,ARLF_exp,'AbsTol',0.001)
+%end
+
+function teardown(testCase)
+% reset local stuff after test
+end
+
+function teardownOnce(testCase)
+% reset global stuff after test
+end
