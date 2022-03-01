02852bc48c63d3b34f57061dfeeb0669b588e8c6
novakge <jsmeqd@gmail.com>
2022-03-01 17:02:44 +0100

    skip zero tasks, improve RD,TD gen. method

--- genpdm.m
+++ genpdm.m
@@ -22,12 +22,10 @@ num_activities = 0;
 for pp=1:p
     num_activities(pp) = n;
 end
-prj_starts = cumsum([1,num_activities(1:end-1)]); % starting with 1 for the first project, ignoring last entry
-prj_ends = cumsum(num_activities(1:end));
 
 % calculate values needed for function call
-num_bits_dsm = p*(n*(n+1)/2); % number of bits considered, only upper triangle including diagonal (=task+relations)
-dsm_possible = 2^num_bits_dsm-1; % including diagonals, excluding all-zero
+num_bits_dsm = p*(n*(n-1)/2); % number of bits considered, only upper triangle excluding diagonal, only relations
+dsm_possible = 2^num_bits_dsm-1; % all possible PDM ids
 
 % prepare all time demand combinations (TD)
 t_elem = length(t_values);
@@ -41,8 +39,8 @@ r_possible = r_elem^r_places;
 
 
 % plausibility checks before creating the matrix
-if dsmid > dsm_possible || dsmid <= 0
-    error('DSM combination %d is out of range [1,%d]!\n',dsmid, dsm_possible);
+if dsmid > dsm_possible || dsmid < 0
+    error('DSM combination %d is out of range [0,%d]!\n',dsmid, dsm_possible);
 end
 if tid > t_possible || tid <= 0
     error('Time combination %d is out of range [1,%d]!\n',tid, t_possible);
@@ -52,69 +50,65 @@ if rid > r_possible || rid <= 0
 end
 
 % create DSM combination
-DSM = zeros(p*n,p*n); % initialize PDM=DSM+RD+TD
+DSM = eye(p*n,p*n); % initialize PDM=DSM+RD+TD
 bits = bitshift(1,num_bits_dsm-1:-1:0); % bitshift for decimal to binary directly
 bits(bits==0) = 0.001; % avoid division by zero with a small epsilon value
-digits = rem(floor(dsmid./bits),2); % create the k-th task+dependency combination
+digits = rem(floor(dsmid./bits),2); % create the k-th dependency combination for n tasks
 
 % create LD combination
 id = 1;
 for prj=1:p
     for tsk=(prj*n)-n+1:prj*n
-        for dep=tsk:prj*n
+        for dep=tsk+1:prj*n
         	DSM(tsk,dep) = digits(id);
             id = id + 1;
         end
     end
 end
-    
-% create TD combination
-TD = zeros(t_places,1);
-t_temp = zeros(t_places,1);
-for i=1:tid
-    TD(:,1) = t_values(t_temp+1);
-    for j=t_places:-1:1
-        t_temp(j,1) = t_temp(j,1)+1;
-        if (t_temp(j,1) == t_elem)
-            t_temp(j,1) = 0;
-        else
-            break;
-        end
-    end
-end
 
+% create TD combination
+TD = permn(t_values,t_places,tid)'; % transpose to column vector
 
 % create RD combination
-RD = zeros(r_places,1); % use rx1 instead of rxr and split later
-r_temp = zeros(r_places,1);
-for k=1:rid
-    RD(:,1) = r_values(r_temp+1);
-    for m=r_places:-1:1
-        r_temp(m,1) = r_temp(m,1)+1;
-        if (r_temp(m,1) == r_elem)
-            r_temp(m,1) = 0;
-        else
-            break;
-        end
-    end
-end
+RD = permn(r_values,r_places,rid); % transpose not necessary
 RD = reshape(RD,[],r); % then split into r columns
 
-% post-processing: remove zero tasks and related values
-% store number of activities before removing zero tasks
-for pp=1:p
-    num_activities(pp) = nnz(diag(DSM(prj_starts(pp):prj_ends(pp),prj_starts(pp):prj_ends(pp))));
-end
-num_activities = nonzeros(num_activities)'; % update number of activities after removing zero tasks
-
-% remove zero tasks and related data
-TD = TD(diag(DSM)==1,:); % remove time demands for zero tasks
-RD = RD(diag(DSM)==1,:); % remove resource demands for zero tasks
-DSM = DSM(diag(DSM)==1,diag(DSM)==1); % finally, remove zero tasks
+% create dummy cost domain
 CD = zeros(size(DSM,1),1);
 
 % return merged PDM
 PDM = [DSM TD CD RD]; % hint: sparse matrix actually takes more storage & computation time
 
-
 end % function
+
+% Old implementations of TD and RD, a bit slower than actual based on permn
+% TD = zeros(t_places,1);
+% t_temp = zeros(t_places,1);
+% for i=1:tid
+%     TD(:,1) = t_values(t_temp+1);
+%     for j=t_places:-1:1
+%         t_temp(j,1) = t_temp(j,1)+1;
+%         if (t_temp(j,1) == t_elem)
+%             t_temp(j,1) = 0;
+%         else
+%             break;
+%         end
+%     end
+% end
+%
+% create RD combination
+% RD = zeros(r_places,1); % use rx1 instead of rxr and split later
+% r_temp = zeros(r_places,1);
+% for k=1:rid
+%     RD(:,1) = r_values(r_temp+1);
+%     
+%     for m=r_places:-1:1
+%         r_temp(m,1) = r_temp(m,1)+1;
+%         if (r_temp(m,1) == r_elem)
+%             r_temp(m,1) = 0;
+%         else
+%             break;
+%         end
+%     end
+% end
+% RD = reshape(RD,[],r); % then split into r columns
